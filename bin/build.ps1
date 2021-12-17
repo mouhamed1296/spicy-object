fd139732558dc613788f0c4220c4806fa0f5fa33
Mamadou Sarr <mbayelel@gmail.com>
2021-12-17 17:02:27 +0100

    Added new feature and bug correction

---
+++ bin/build.ps1
@@ -0,0 +1,4 @@
+tsc -p .
+npx google-closure-compiler --js=lib/index.js --js_output_file=lib/index.min.js
+Remove-Item -Path ./lib/index.js
+Move-Item -Path ./lib/index.min.js -Destination ./lib/index.js
