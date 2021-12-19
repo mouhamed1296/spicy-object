4efeb3cc3c22cc78ae4095db262eeb708d91d26d
Mamadou Sarr <mbayelel@gmail.com>
2021-12-19 18:11:36 +0100

    "removing the slice method and getEntryByIndex method"

--- bin/build.ps1
+++ bin/build.ps1
@@ -2,3 +2,6 @@ tsc -p .
 npx google-closure-compiler --js=lib/index.js --js_output_file=lib/index.min.js
 Remove-Item -Path ./lib/index.js
 Move-Item -Path ./lib/index.min.js -Destination ./lib/index.js
+git add .
+git commit
+git push origin master
